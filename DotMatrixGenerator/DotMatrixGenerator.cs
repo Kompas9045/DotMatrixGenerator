038ad924caa658074cb6bb6f27df5649b04c3a1c
KOMPAS9045 <gujiabin2005@foxmail.com>
2025-05-21 22:10:13 +0800

    initialize

--- DotMatrixGenerator/DotMatrixGenerator.cs
+++ DotMatrixGenerator/DotMatrixGenerator.cs
@@ -1,6 +1,7 @@
 /*
  <DotMatrixGenerator>
 Developer:Gu JiaBin
+https://github.com/Kompas9045/DotMatrixGenerator.git
  
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lattice
+# DotMatrixGenerator
+
+This is DotMatrixGenerator, which allows converting images or videos into dot matrix images, supports raster images and vector formats. This is the small tool I used in the After Effect course
